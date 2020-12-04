2a6d7258c37831cc171111836f114d567d38b7ad
Manoel Neto <maguserv@gmail.com>
2020-12-04 16:44:37 -0300

    Add end Configure Jest

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import userRoutes from '@modules/users/routes/UsersRoutes';
 import sessionRoutes from '@modules/users/routes/SessionRoutes';
 
 const routes = Router();
-const prefixRoutes = '/api/v1';
+const prefixRoutes = '/v1';
 
 routes.use(`${prefixRoutes}`, userRoutes);
 routes.use(`${prefixRoutes}`, sessionRoutes);
