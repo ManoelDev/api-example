f316b4ca8ae9a95a3f9855f069def50751981de6
Manoel Neto <maguserv@gmail.com>
2020-12-05 16:33:40 -0300

    Edit prefix vesion

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userRoutes from '@modules/users/routes/UsersRoutes';
 import sessionRoutes from '@modules/users/routes/SessionRoutes';
+import categoryRouytes from '@modules/Categories/routes/CreateCategoryRoutes';
 
 const routes = Router();
-const prefixRoutes = '/v1';
+const prefixV1 = '/v1';
 
-routes.use(`${prefixRoutes}`, userRoutes);
-routes.use(`${prefixRoutes}`, sessionRoutes);
+routes.use(`${prefixV1}`, userRoutes);
+routes.use(`${prefixV1}`, sessionRoutes);
+routes.use(`${prefixV1}`, categoryRouytes);
 
 export default routes;
